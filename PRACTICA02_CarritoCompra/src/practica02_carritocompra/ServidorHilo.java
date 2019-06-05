e37ac00b173375967acce81b6ad986aa8f3cdaaf
iangelmx <aramireza1506@alumno.ipn.mx>
2019-06-05 00:10:01 -0500

    Se empieza a hacer práctica multicliente

---
+++ PRACTICA02_CarritoCompra/src/practica02_carritocompra/ServidorHilo.java
@@ -0,0 +1,159 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica02_carritocompra;
+
+import com.google.gson.Gson;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ *
+ * @author iAngelMx
+ */
+public class ServidorHilo extends Thread{
+    
+    private Socket socket;
+    private int idSessio;
+    DataOutputStream dos;
+    DataInputStream dis;
+    InputStream inFile = null,inSocket=null;
+    OutputStream outSocket = null,outFile=null;
+    private Gson json;
+    private int buffer;
+    ServerSocket serverSocket = null;
+    
+    public ServidorHilo(Socket socket, int id, int buffer) {
+        this.socket = socket;
+        this.idSessio = id;
+        try {
+            dos = new DataOutputStream(socket.getOutputStream());
+            dis = new DataInputStream(socket.getInputStream());
+        } catch (IOException ex) {
+            System.out.println("Hubo una excepción en __Constructor__: "+ex);
+        }
+        this.json= new Gson();
+        this.buffer = buffer;
+    }
+    
+    public void desconnectar() {
+        try {
+            socket.close();
+        } catch (IOException ex) {
+            System.out.println("Hubo una excepción en desconectar: "+ex);
+        }
+    }
+    
+    public Item[] getInventario () throws IOException{
+        FileInputStream fstream = new FileInputStream("src\\inventario\\inventario.json");
+        BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+        String line;
+
+        Gson gson= new Gson();
+        Item[] items=new Item[10];
+        int a = 0;
+        while ((line = br.readLine()) != null) {
+           items = gson.fromJson(line,Item[].class);
+           System.out.println(line);
+           a+=1;
+        }
+        return items;
+    }
+    
+    public void setInventario(String lista) throws IOException{
+        InputStream inFile=null;
+        OutputStream outFile=null;
+        int i=0;
+        Item[] inventarioActual =getInventario();
+        Item[] listaCliente=null;
+        inFile=new FileInputStream(lista);
+        BufferedReader br = new BufferedReader(new InputStreamReader(inFile));
+        String line;
+        Gson gson= new Gson();
+        
+        while ((line = br.readLine()) != null) {
+           listaCliente = gson.fromJson(line,Item[].class);
+        }
+        
+        for(Item productoC:listaCliente){
+            for(Item productoS: inventarioActual){
+            
+                if(productoC.sku.equals(productoS.sku)){
+                    productoS.exis-=productoC.exis;
+                    inventarioActual[i]=productoS;
+                    
+                }
+                i++;
+            }
+            
+            i=0;
+        
+        }
+        
+        outFile = new FileOutputStream("src\\inventario\\inventario.json");
+        byte[] bytes = new byte[30*1024];
+        int count=0;
+        inFile=new ByteArrayInputStream(gson.toJson(inventarioActual).getBytes());
+        while ((count = inFile.read(bytes)) > 0) {
+                        outFile.write(bytes, 0, count);   
+                        System.out.println("linea "+i+". Cuenta servidor: "+count);
+                    }
+    
+    }
+   
+    
+    @Override
+    public void run(){
+        int count;
+        try {
+            boolean flag = false;
+            if( flag == true){
+                byte[] bytes = new byte[this.buffer * 1024];
+                System.out.println(this.json.toJson(getInventario()));
+                inFile = new ByteArrayInputStream(this.json.toJson(getInventario()).getBytes());
+
+
+                outSocket = socket.getOutputStream();
+                while ((count = inFile.read(bytes)) > 0) {
+                    outSocket.write(bytes, 0, count);
+                }
+                //outSocket.flush();
+                socket.close();
+                System.out.println("Se supone que lo envió");
+                flag=true;
+            }else{
+                try{
+                    socket = serverSocket.accept();  
+                    inSocket = socket.getInputStream();
+                    System.out.println("Trató de obtener un dato de entrada");
+                    outFile = new FileOutputStream("src\\inventario\\recibidoDeCliente.json");
+                    byte[] bytes = new byte[buffer*1024];
+                    int i=0;
+                    String msg;
+                    System.out.println("Esperará archivo");
+                    while ((count = inSocket.read(bytes)) > 0) {
+                        outFile.write(bytes, 0, count);
+                        i++;
+                        System.out.println("linea "+i+". Cuenta servidor: "+count);
+                    }
+                    
+                    System.out.println("Salió del while");
+                    setInventario("src\\inventario\\recibidoDeCliente.json");
+                    flag=false;
+                    }
+                    catch(Exception ex){
+                        //System.out.println("Excepción Else Servidor: "+ex);
+                    }
+            }
+            
+            
+        } catch (IOException ex) {
+            System.out.println("Hubo una excepción en run(): "+ex);
+        }
+        desconnectar();
+    }
+    
+}
